6219c4f09930a7f983ff6183ea8064b3fa72df1f
nmccready <nemtcan@gmail.com>
2023-01-04 15:18:00 -0500

    fix: locking shdoc to v1.1 and not master

--- prepublish.sh
+++ prepublish.sh
@@ -3,12 +3,12 @@ set -e -o pipefail
 
 prepublish() {
   if [[ ! -d shdoc ]]; then
-    git clone https://github.com/reconquest/shdoc.git
+    curl -OL https://github.com/reconquest/shdoc/releases/download/v1.1/shdoc
   fi
 
   # make the os env find the gawk installation
   # https://github.com/reconquest/shdoc/issues/24
-  sed "s/\#\!\/usr\/bin\/gawk -f/\#\!\/usr\/bin\/env gawk -f/" shdoc/shdoc > dist/shdoc
+  sed "s/\#\!\/usr\/bin\/gawk -f/\#\!\/usr\/bin\/env gawk -f/" shdoc > dist/shdoc
   chmod +x dist/shdoc
 }
 #if sourced abort / exit
