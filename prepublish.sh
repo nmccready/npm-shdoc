a61d36d16ef6c9a23ae28affcc6daa42eca4ce4c
nmccready <nemtcan@gmail.com>
2023-12-18 14:15:19 -0500

    feat: support new versions of shdoc

--- prepublish.sh
+++ prepublish.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
+SHDOC_VERSION=$(jq -r ".shellDependencies.shdoc" package.json)
+# echo SHDOC_VERSION: $SHDOC_VERSION
 prepublish() {
   if [[ ! -d shdoc ]]; then
-    curl -OL https://github.com/reconquest/shdoc/releases/download/v1.1/shdoc
+    curl -L https://github.com/reconquest/shdoc/archive/refs/tags/v${SHDOC_VERSION}.tar.gz | tar -xzv
+    mv "shdoc-${SHDOC_VERSION}/shdoc" shdoc
+    rm -rf "shdoc-${SHDOC_VERSION}"
   fi
 
   # make the os env find the gawk installation
