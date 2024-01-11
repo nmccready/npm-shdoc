158326e4416dd1d927b297f15e7a83c08c38937b
nmccready <nemtcan@gmail.com>
2024-01-11 11:55:02 -0500

    fix: prepublish force shdoc download

--- prepublish.sh
+++ prepublish.sh
@@ -4,11 +4,12 @@ set -e -o pipefail
 SHDOC_VERSION=$(jq -r ".shellDependencies.shdoc" package.json)
 # echo SHDOC_VERSION: $SHDOC_VERSION
 prepublish() {
-  if [[ ! -d shdoc ]]; then
-    curl -L https://github.com/reconquest/shdoc/archive/refs/tags/v${SHDOC_VERSION}.tar.gz | tar -xzv
-    mv "shdoc-${SHDOC_VERSION}/shdoc" shdoc
-    rm -rf "shdoc-${SHDOC_VERSION}"
-  fi
+  # always download the latest version of shdoc
+  rm -rf shdoc dist
+  mkdir -p dist
+  curl -L https://github.com/reconquest/shdoc/archive/refs/tags/v${SHDOC_VERSION}.tar.gz | tar -xzv
+  mv "shdoc-${SHDOC_VERSION}/shdoc" shdoc
+  rm -rf "shdoc-${SHDOC_VERSION}"
 
   # make the os env find the gawk installation
   # https://github.com/reconquest/shdoc/issues/24
